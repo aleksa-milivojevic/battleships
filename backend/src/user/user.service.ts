12325cdce055a015cc661692265194c5d25637d3
aleksa-milivojevic <milivojevic.aki@gmail.com>
2026-08-02 19:51:47 +0200

    dodati user.service i user.controller fajlovi

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) {}
+
+    findAll(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+    findOne(id: number): Promise<User | null> {
+        return this.userRepository.findOneBy({ id });
+    }
+
+    async removeOne(id: number): Promise<void> {
+        await this.userRepository.delete({ id });
+    }
+}
