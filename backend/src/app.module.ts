b51bda6d6a5b7f3e4fe071d7ea6b3012b0710c97
aleksa-milivojevic <milivojevic.aki@gmail.com>
2026-08-02 19:03:03 +0200

    dodat typeORMmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: 'root',
+      password: 'aleksA04',
+      database: 'first_db',
+      entities: [User, Match, Queue, Report],
+      synchronize: false,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
