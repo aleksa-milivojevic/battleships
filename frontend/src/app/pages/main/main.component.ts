fd13ef34c566aa9151b1aa2a2458c8d75e2a4c00
aleksa-milivojevic <milivojevic.aki@gmail.com>
2026-08-06 13:00:51 +0200

    dodata inicijalizacija sidebar komponente

--- frontend/src/app/pages/main/main.component.ts
+++ frontend/src/app/pages/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
