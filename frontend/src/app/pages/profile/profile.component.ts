f44e28cba42ecaef85ad33454dd4ecdd238860ae
aleksa-milivojevic <milivojevic.aki@gmail.com>
2026-08-10 21:39:43 +0200

    dodata promena username-a na stranicu profile

--- frontend/src/app/pages/profile/profile.component.ts
+++ frontend/src/app/pages/profile/profile.component.ts
@@ -1,24 +1,53 @@
-import { Component, OnInit, inject, signal } from "@angular/core";
+import { Component, OnInit, computed, inject, signal } from "@angular/core";
 import { SidebarComponent } from "../../shared/sidebar/sidebar.component";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
-import { User } from "../../services/user.service";
+import { UserService } from "../../services/user.service";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [SidebarComponent, FormsModule],
+  imports: [SidebarComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
 export class ProfileComponent implements OnInit {
     private authService = inject(AuthService);
-    
-    self = signal<User | null>(null);
+    private userService = inject(UserService);
 
-    ngOnInit(): void {
-        this.self.set(this.authService.user());
+    self = this.authService.user;
+
+    newUsername = signal('');
+    loading = signal(false);
+    errorMessage = signal('');
+    showChNameScreen = signal(false);
+
+    validUsername = computed(() => {
+        return this.newUsername().length >= 3
+    })
+
+    ngOnInit(): void {}
+
+    onChName(): void {
+        if (!this.validUsername) return;
+
+        this.loading.set(true);
+
+        this.userService.changeUsername(this.self()?.id!, this.newUsername()).subscribe({
+            next: (res) => {
+                console.log(res.user);
+                this.loading.set(false);
+                this.toggleChNameScreen();
+            },
+            error: (err) => {
+                console.error(err);
+                this.loading.set(false);
+                this.errorMessage.set('Serverska greska');
+            }
+        });
     }
 
-
+    toggleChNameScreen() {
+        this.showChNameScreen.update(v => !v);
+    }
 }
