e6ea10c1bc5c2709e7927d9e24743d825bc58528
aleksa-milivojevic <milivojevic.aki@gmail.com>
2026-08-22 21:39:09 +0200

    izbacen queue entity i sve povezano, queue se radi preko socketa, dodati queue.gateway i queue.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,15 +4,14 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './database/user/user.entity';
 import { Match } from './database/match/match.entity';
-import { Queue } from './database/queue/queue.entity';
 import { Report } from './database/report/report.entity';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './database/user/user.module';
 import { MatchModule } from './database/match/match.module';
-import { QueueModule } from './database/queue/queue.module';
 import { ReportModule } from './database/report/report.module';
 import { AuthModule } from './auth/auth.module';
 import { ChallangeModule } from './sockets/challange/challange.module';
+import { QueueModule } from './sockets/queue/queue.module';
 
 @Module({
   imports: [
@@ -26,15 +25,15 @@ import { ChallangeModule } from './sockets/challange/challange.module';
       username: 'root',
       password: process.env.DB_PASSWORD,
       database: 'first_db',
-      entities: [User, Match, Queue, Report],
+      entities: [User, Match, Report],
       synchronize: false,
     }),
     UserModule,
     MatchModule,
-    QueueModule,
     ReportModule,
     AuthModule,
-    ChallangeModule
+    ChallangeModule,
+    QueueModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/sockets/queue/queue.gateway.ts
@@ -0,0 +1,37 @@
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { Socket } from "socket.io";
+
+@WebSocketGateway(3002, { cors: { origin: 'http://localhost:4200', credentials: true } })
+export class QueueGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    
+    private queue = new Map<string,Socket>();
+    private ctu = new Map<string,string>();
+
+    handleConnection(@ConnectedSocket() client: Socket) {
+        setTimeout(() => client.emit('id-req'), 500);
+    }
+    
+    handleDisconnect(@ConnectedSocket() client: Socket) {
+        const id = this.ctu.get(client.id);
+        this.ctu.delete(client.id);
+        this.queue.delete(id!);
+    }
+
+    @SubscribeMessage('id-res')
+    matchmaking(@MessageBody('id') id: string, @ConnectedSocket() client: Socket) {
+        if (this.queue.size === 0) {
+            this.queue.set(id, client);
+            this.ctu.set(client.id, id);
+            client.emit('in-queue');
+        }
+        else {
+            const [oppId, oppSocket] = this.queue.entries().next().value;
+
+            oppSocket.emit('match-found', { oppId: id });
+            client.emit('match-found', { oppId: oppId });
+        
+            this.queue.delete(oppId);
+            this.ctu.delete(oppSocket.id);
+        }
+    }
+}
