fca9e64e12415f54bd61ab6804b5a9880f2217d9
aleksa-milivojevic <milivojevic.aki@gmail.com>
2026-09-02 12:48:06 +0200

    cuvanje u local storage za bot-game.ts

--- frontend/src/app/pages/bot-game/bot-game.component.ts
+++ frontend/src/app/pages/bot-game/bot-game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, effect, inject, signal } from "@angular/core";
 import { BotService } from "../../services/bot.service";
 import { StorageService } from "../../services/storage.service";
 import { Router } from "@angular/router";
+import { ignoreElements } from "rxjs";
 
 @Component({
     selector: 'app-bot-game',
@@ -30,16 +31,48 @@ export class BotGameComponent implements OnInit {
         this.field.set(this.storage.getItem('MY_FIELD') ?? this.field());
         this.oppField.set(this.storage.getItem('OPP_FIELD') ?? this.oppField());
         this.myMove.set(this.storage.getItem('MY_MOVE') ?? true);
+        this.gameOver.set(this.storage.getItem('GAME_OVER') ?? false);
+        this.gameOverScreen.set(this.storage.getItem('GO_SCREEN') ?? false);
+        this.win.set(this.storage.getItem('WIN') ?? false);
+        this.fieldError.set(this.storage.getItem('FIELD_ERR') ?? '');
+        this.surrenderMessage.set(this.storage.getItem('SURR_MSG') ?? '');
+
         effect(() => {
             this.myMove();
             if (this.myMove() === false) {
                 this.botMove();
             }
+            this.storage.setItem('MY_MOVE', this.myMove());
         });
         effect(() => {
             this.gameOver();
             this.toggleGameOverScreen();
-        })
+            this.storage.setItem('GAME_OVER', this.gameOver());
+        });
+        effect(() => {
+            this.field();
+            this.storage.setItem('MY_FIELD', this.field());
+        });
+        effect(() => {
+            this.oppField();
+            this.storage.setItem('OPP_FIELD', this.oppField());
+        });
+        effect(() => {
+            this.gameOverScreen();
+            this.storage.setItem('GO_SCREEN', this.gameOverScreen());
+        });
+        effect(() => {
+            this.win();
+            this.storage.setItem('WIN', this.win());
+        });
+        effect(() => {
+            this.fieldError();
+            this.storage.setItem('FIELD_ERR', this.fieldError());
+        });
+        effect(() => {
+            this.surrenderMessage();
+            this.storage.setItem('SURR_MSG', this.surrenderMessage());
+        });
     }
 
     ngOnInit(): void {}
@@ -50,7 +83,6 @@ export class BotGameComponent implements OnInit {
             else f[i][j] = 0;
             return f;
         });
-        this.storage.setItem('MY_FIELD', this.field());
     }
 
     onReadyUp() {
@@ -131,5 +163,10 @@ export class BotGameComponent implements OnInit {
         this.storage.removeItem('MY_FIELD');
         this.storage.removeItem('OPP_FIELD');
         this.storage.removeItem('MY_MOVE');
+        this.storage.removeItem('GAME_OVER');
+        this.storage.removeItem('GO_SCREEN');
+        this.storage.removeItem('WIN');
+        this.storage.removeItem('FIELD_ERR');
+        this.storage.removeItem('SURR_MSG');
     }
 }
