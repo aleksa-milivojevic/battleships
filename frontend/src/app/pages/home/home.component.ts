df76cd9c5d931d17e9a40240a95bd848c215a695
aleksa-milivojevic <milivojevic.aki@gmail.com>
2026-09-08 17:04:30 +0200

    dodate poruke  u home za ban/timeout

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,28 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  private storage = inject(StorageService);
+
+  banned = signal(false);
+  timeout = signal<number | null>(null);
+  expiration = signal(new Date());
+
+  constructor() {
+    this.banned.set(this.storage.getItem('BAN') ?? false);
+    this.timeout.set(this.storage.getItem('TIMEOUT') ?? null);
+
+    if (this.banned()) {
+      this.storage.removeItem('BAN');
+    }
+
+    if (this.timeout()) {
+      this.storage.removeItem('TIMEOUT');
+      this.expiration.update(date => {
+        let copy = new Date(date);
+        copy.setMinutes(copy.getMinutes() + this.timeout()!);
+        return copy;
+      })
+    }
+  }
+}
